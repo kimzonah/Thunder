52ed59153340a54c12c0c29f7a403fac53938d44
jihye <c0zl@hyeee19@naver.com>
2024-05-16 21:43:22 +0900

    feat. 전체 게시글 조회 및 mapper명 변경

--- Thunder/src/main/java/com/thunder/model/dto/Board.java
+++ Thunder/src/main/java/com/thunder/model/dto/Board.java
@@ -1,5 +1,56 @@
 package com.thunder.model.dto;
 
-public class Board {
+import java.sql.Timestamp;
 
+public class Board {
+	private int id;
+	private String title;
+	private String content;
+	private String userId;
+	private Timestamp regDate;
+	private int scheduleId;
+	private int commentCnt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+	public int getScheduleId() {
+		return scheduleId;
+	}
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
+	}
+	public int getCommentCnt() {
+		return commentCnt;
+	}
+	public void setCommentCnt(int commentCnt) {
+		this.commentCnt = commentCnt;
+	}
 }
--- Thunder/src/main/java/com/thunder/model/service/BoardService.java
+++ Thunder/src/main/java/com/thunder/model/service/BoardService.java
@@ -1,5 +1,11 @@
 package com.thunder.model.service;
 
+import java.util.List;
+
+import com.thunder.model.dto.Board;
+
 public interface BoardService {
+
+	List<Board> getAllBoard(int scheduleId);
 
 }
