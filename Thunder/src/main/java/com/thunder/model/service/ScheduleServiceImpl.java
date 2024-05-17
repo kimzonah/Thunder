254e0bcc3653a7a04c27bafd439063148ba5897c
최승탁 <tmdxkr5@hanmail.net>
2024-05-17 16:33:41 +0900

    feat. 번개 생성 기능 완료

--- Thunder/src/main/java/com/thunder/model/service/ScheduleServiceImpl.java
+++ Thunder/src/main/java/com/thunder/model/service/ScheduleServiceImpl.java
@@ -1,12 +1,19 @@
 package com.thunder.model.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.thunder.model.dao.ScheduleDao;
 import com.thunder.model.dto.Schedule;
@@ -14,31 +21,62 @@ import com.thunder.model.dto.ScheduleSearchCondtion;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
-	
+
 	private final ScheduleDao scheduleDao;
-	
+	private final ResourceLoader resourceLoader;
+	private static final String DEFALT_SCHEDULE_IMAGE = "default_schedule.jpg";
+
 	@Autowired
-	public ScheduleServiceImpl(ScheduleDao scheduleDao) {
+	public ScheduleServiceImpl(ScheduleDao scheduleDao, ResourceLoader resourceLoader) {
 		this.scheduleDao = scheduleDao;
+		this.resourceLoader = resourceLoader;
 	}
-	
+
 	@Override
-    public List<Schedule> getScheduleList(String dateTime, String category, String addressName) {
-        Map<String, String> condition = new HashMap<>();
-        
-        condition.put("dateTime", dateTime);
-        condition.put("category",category);
-        condition.put("addressName", addressName);
-        
-        return scheduleDao.selectScheduleByCon(condition);
-    }
-	
+	public List<Schedule> getScheduleList(String dateTime, String category, String addressName) {
+		Map<String, String> condition = new HashMap<>();
+
+		condition.put("dateTime", dateTime);
+		condition.put("category", category);
+		condition.put("addressName", addressName);
+
+		return scheduleDao.selectScheduleByCon(condition);
+	}
+
 	// 번개 상세 조회
 	@Override
 	public Schedule getOneSchedule(int scheduleId) {
 		return scheduleDao.selectOneSchedule(scheduleId);
 	}
-	
-	
-	
+
+	// 번개 생성
+	@Transactional
+	@Override
+	public int createSchedule(Schedule schedule, MultipartFile file) {
+		// 업로드한 파일 있으면
+		if (file != null && file.getSize() > 0) {
+			try {
+				// 원본 파일명 저장
+				schedule.setOrgImage(file.getOriginalFilename());
+				// 중복 방지 파일명 저장
+				schedule.setImage(UUID.randomUUID().toString() + "_" + file.getOriginalFilename());
+
+				Resource resource = resourceLoader.getResource("classpath:/static/img");
+				file.transferTo(new File(resource.getFile(), schedule.getImage()));
+
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// 업로드한 파일 없으면 기본 이미지 저장
+		else {
+			schedule.setOrgImage(DEFALT_SCHEDULE_IMAGE);
+			// 중복 방지 파일명 저장
+			schedule.setImage(UUID.randomUUID().toString() + "_" + DEFALT_SCHEDULE_IMAGE);
+		}
+		return scheduleDao.insertSchedule(schedule);
+
+	}
+
 }
