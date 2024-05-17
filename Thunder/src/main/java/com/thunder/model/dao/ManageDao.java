0c958c1c70b9462149b14326d486b1607f57ba3c
jihye <hyeee19@naver.com>
2024-05-17 14:47:11 +0900

    Feat. 매니저 번개관리 기능

--- Thunder/src/main/java/com/thunder/model/dao/ManageDao.java
+++ Thunder/src/main/java/com/thunder/model/dao/ManageDao.java
@@ -1,5 +1,21 @@
 package com.thunder.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.thunder.model.dto.Schedule;
+import com.thunder.model.dto.User;
+
 public interface ManageDao {
+
+	int validate(Map<String, Object> params);
+
+	List<User> selectAllApplyUser(int scheduleId);
+
+	int update(Map<String, Object> params);
+
+	int delete(Map<String, Object> params);
+
+	int updateSchedule(Schedule schedule);
 
 }
