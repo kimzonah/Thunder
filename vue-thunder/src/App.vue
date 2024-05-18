6f94b0f859910c7fffa32c043731d9bb1f386d8a
jihye <c0zl@hyeee19@naver.com>
2024-05-18 17:48:53 +0900

    feat. vue 초기세팅

---
+++ vue-thunder/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import TheHeaderNav from '@/components/common/TheHeaderNav.vue';
+</script>
+
+<template>
+  <TheHeaderNav />
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ vue-thunder/src/components/common/TheHeaderNav.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>The Header Nav</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
